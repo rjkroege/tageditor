7b40fcd002c63c83befb9da4620a810e52d0b984
Robert Kroeger <rjkroege@liqui.org>
2023-05-17 15:09:03 -0400

    Switched to new Swift implementation

---
+++ Sources/tageditor/tageditor.swift
@@ -0,0 +1,10 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+print("hello!")
+
+func sayHello(name: String) {
+    print("Hello, \(name)!")
+}
+
+sayHello(name: "Rob")
--- go.mod
+++
@@ -1,9 +0,0 @@
-module github.com/rjkroege/tageditor
-
-go 1.20
-
-require github.com/frolovo22/tag v0.0.2
-
-replace (
-	github.com/frolovo22/tag v0.0.2 => ../_builds/tag
-)
